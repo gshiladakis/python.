c3b79329e031dea3dff5492db5744d4f8a7adabb
gshiladakis <66285313+gshiladakis@users.noreply.github.com>
2026-02-27 11:45:32 -0500

    Add test score average calculation
    Fix calculation of average score and output formatting.

---
+++ test_score_average.py
@@ -0,0 +1,12 @@
+# Get three test scores and assign them to the 
+# test1, test2, and test3 variables.
+test1 = input('Enter the first test score: ')
+test2 = input('Enter the second test score: ')
+test3 = input('Enter the third test score: ')
+
+# Calcualte the average of the three scores
+# and assign the result to the average variable.
+average = (test1 + test2 + test3) / 3.0
+
+# Display the average.
+print 'The averager score is', average 
